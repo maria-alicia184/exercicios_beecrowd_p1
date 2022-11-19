40e60ac019dce2374d9bc3d4a168a9f587155f14
Maria Alicia <maria.alicia184@outlook.com>
2022-11-19 00:10:55 -0300

    Exercicio 04: Cálculo raízes de Bhaskara

---
+++ FormulaDeBhaskara.java
@@ -0,0 +1,20 @@
+public class FormulaDeBhaskara {
+    public static void main(String [] args){
+        double r1;
+        double r2;
+        double a = 10.3;
+        double b = 203.0;
+        double c = 5.0;
+        double delta = (b*b) - 4*a*c;
+
+        r1 = ((b*-1) + Math.sqrt(delta))/(2*a);
+        r2 = ((b*-1) - Math.sqrt(delta))/(2*a);
+
+        if(a != 0 && delta >= 0){
+            System.out.printf("R1 = %.5f\n" , r1);
+            System.out.printf("R2 = %.5f\n", r2);
+        }
+        else{
+        System.out.println("Impossível calcular");}
+    }
+}
